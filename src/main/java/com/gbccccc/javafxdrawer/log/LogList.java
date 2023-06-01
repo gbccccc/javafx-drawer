9c96a2ad10e6b24c6c1c72afe3ab85023d694f9e
gbccccc <78393470+gbccccc@users.noreply.github.com>
2023-06-01 12:47:32 +0800

    LogList

--- src/main/java/com/gbccccc/javafxdrawer/log/LogList.java
+++ src/main/java/com/gbccccc/javafxdrawer/log/LogList.java
@@ -1,4 +1,47 @@
 package com.gbccccc.javafxdrawer.log;
 
+import java.util.*;
+
 public class LogList {
+    private final Stack<Log> dones;
+    private final Stack<Log> todos;
+
+    private LogList() {
+        this.dones = new Stack<>();
+        this.todos = new Stack<>();
+    }
+
+    // lazy singleton holder
+    private static final class LogListHolder {
+        private static final LogList logList = new LogList();
+    }
+
+    public static LogList getLogList() {
+        return LogListHolder.logList;
+    }
+
+    public void undo() {
+        if (dones.empty()) {
+            return;
+        }
+
+        Log log = dones.pop();
+        log.undo();
+        todos.push(log);
+    }
+
+    public void redo() {
+        if (todos.empty()) {
+            return;
+        }
+
+        Log log = todos.pop();
+        log.redo();
+        dones.push(log);
+    }
+
+    public void addLog(Log log) {
+        dones.push(log);
+        todos.clear();
+    }
 }
